b154ef1080848f035a9ecc30407164742bab6db3
Nutter Fi <itsnutterfi@gmail.com>
2022-06-02 20:47:10 -0700

    Initial Commit

---
+++ FaceAvatar/FaceAvatarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceAvatarApp.swift
+//  FaceAvatar
+//
+//  Created by nutterfi on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FaceAvatarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
